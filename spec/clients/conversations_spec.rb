4988bc2d59d4af4665a42d0d14799cc2e3db1f99
Andrew Mason <andrewmcodes@protonmail.com>
2020-10-16 11:07:15 -0400

    chore: update json responses

--- spec/clients/conversations_spec.rb
+++ spec/clients/conversations_spec.rb
@@ -8,31 +8,100 @@ RSpec.describe Structurely::Clients::Conversations do
   end
 
   describe "#show" do
-    it "return the record when the record exists" do
-      stub = stub_request(:get, /conversations/).to_return(status: 200, body: load_json("conversations_get").to_json)
+    context "when the record exists" do
+      it "returns success" do
+        stub = stub_request(:get, /conversations/).to_return(status: 200, body: load_json("conversations_get").to_json)
+        response = subject.show("5f89abd220c8880031b975cc")
 
-      response = subject.show("5c09a4416241ea2c293275b8")
-
-      expect(stub).to have_been_requested
-      expect(response.success?).to be_truthy
+        expect(stub).to have_been_requested
+        expect(response.success?).to be_truthy
+        expect(response.failure?).to be_falsey
+      end
     end
 
-    it "returns nil when the record does not exist" do
-      stub = stub_request(:get, /conversations/)
-        .to_return(status: 404, body: {
-          code: 404, message: "Conversation does not exist"
-        }.to_json)
+    context "when the record does not exist" do
+      it "returns failure" do
+        stub = stub_request(:get, /conversations/).to_return(status: 404, body: {code: 404, message: "Conversation does not exist"}.to_json)
+        response = subject.show("foobar")
 
-      response = subject.show("foobar")
-
-      expect(stub).to have_been_requested
-      expect(response.success?).to be_falsey
-      expect(response.failure).not_to be_nil
-
-      expect(response.failure.status).to eq(404)
+        expect(stub).to have_been_requested
+        expect(response.success?).to be_falsey
+        expect(response.failure?).to be_truthy
+      end
     end
   end
 
   describe "#create" do
+    context "when the record is persisted" do
+      it "returns success" do
+        stub = stub_request(:post, /conversations/).to_return(status: 201, body: load_json("conversations_post").to_json)
+
+        response = subject.create(
+          muted: false,
+          settings: Structurely::Entities::ConversationSettings.new(
+            time_zone: "America/Chicago"
+          ),
+          slots: [
+            Structurely::Entities::ConversationSlot.new(
+              name: "email",
+              value: "jdoe@example.com"
+            )
+          ],
+          messages: [
+            Structurely::Entities::ConversationItem.new(
+              response: Structurely::Entities::ConversationMessage.new(
+                text: "Hello, what is your name?",
+                received: "2018-12-08T15:20:00.000Z"
+              ),
+              context: "expect_name",
+              message: Structurely::Entities::ConversationMessage.new(
+                text: "John",
+                received: "2018-12-08T16:34:00.000Z"
+              )
+            )
+          ]
+        )
+
+        expect(stub).to have_been_requested
+        expect(response.success?).to be_truthy
+        expect(response.failure?).to be_falsey
+      end
+    end
+
+    context "when the record is not persisted" do
+      it "returns failure" do
+        stub = stub_request(:post, /conversations/).to_return(status: 400, body: load_json("conversations_post").to_json)
+
+        response = subject.create(
+          muted: false,
+          settings: Structurely::Entities::ConversationSettings.new(
+            time_zone: "America/Chicago"
+          ),
+          slots: [
+            Structurely::Entities::ConversationSlot.new(
+              name: "email",
+              value: "jdoe@example.com"
+            )
+          ],
+          messages: [
+            Structurely::Entities::ConversationItem.new(
+              response: Structurely::Entities::ConversationMessage.new(
+                text: "Hello, what is your name?",
+                received: "2018-12-08T15:20:00.000Z"
+              ),
+              context: "expect_name",
+              message: Structurely::Entities::ConversationMessage.new(
+                text: "John",
+                received: "2018-12-08T16:34:00.000Z"
+              )
+            )
+          ]
+        )
+
+        expect(stub).to have_been_requested
+        expect(response.success?).to be_falsey
+        expect(response.failure?).to be_truthy
+      end
+    end
   end
 end
