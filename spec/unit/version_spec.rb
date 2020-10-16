c5d84fd281a1cde487f529a0db9c2385b9bda95a
Andrew Mason <andrewmcodes@protonmail.com>
2020-10-16 12:43:42 -0400

    test: set up rspec testing workflow (#4)
    - Add `.env.example`
    - Add GitHub action for running tests
    - Configure RSpec
    - Add RSpec binstub
    - Small enhancement to setup binstub

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Structurely do
   it "has a version number" do
     expect(Structurely::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
